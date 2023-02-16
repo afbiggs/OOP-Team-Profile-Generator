ab80c4711c8fe9230aac0f8f7bbe1eebbb6ba216
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-02-16 03:47:24 -0500

    employee tests passed

---
+++ __tests__/employee.test.js
@@ -0,0 +1,51 @@
+
+const Employee = require("../lib/Employee");
+
+test("Can create an new employee.", () => {
+    const employeeInstance = new Employee();
+    expect(typeof(employeeInstance)).toBe("object");
+})
+
+test("Testing name.", () => {
+    const name = "Alex";
+    const employeeInstance = new Employee(name);
+    expect(employeeInstance.name).toBe(name);
+})
+
+test("Testing ID.", () => {
+    const id = 47;
+    const employeeInstance = new Employee("Alex", id);
+    expect(employeeInstance.id).toBe(id);
+})
+
+test("Testing email.", () => {
+    const email = "afbiggs73@gmail.com";
+    const employeeInstance = new Employee("Alex", 2, email);
+    expect(employeeInstance.email).toBe(email);
+})
+
+
+
+test("Gets name through getName method.", () => {
+    const testName = "Alex";
+    const employeeInstance = new Employee(testName);
+    expect(employeeInstance.getName()).toBe(testName);
+})
+
+test("Can test ID through getID method.", () => {
+    const testID = 47;
+    const employeeInstance = new Employee("Alex", testID);
+    expect(employeeInstance.getId()).toBe(testID);
+})
+
+test("Can test email through getEmail method.", () => {
+    const testEmail = "afbiggs73@gmail.com";
+    const employeeInstance = new Employee("Alex", 47, testEmail);
+    expect(employeeInstance.getEmail()).toBe(testEmail);
+})
+
+test("Testing role.", () => {
+    const returnValue = "Employee";
+    const employeeInstance = new Employee("Alex", 47, "afbiggs73@gmail.com");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+})
